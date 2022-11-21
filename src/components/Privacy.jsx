f7ed30db2dc50ddc57624b6bac2825047af8bfce
Akhila H <Akhilah@er.ai>
2022-11-21 20:04:48 +0530

    privacy_task3

---
+++ src/components/Privacy.jsx
@@ -0,0 +1,174 @@
+import React from 'react';
+import "./privacy.css";
+import vector from "./image/Vector.svg"
+
+
+
+function Privacy() {
+  return (
+    <div className='body'>
+      <div className='bg'>
+        <div className='dripp'>
+          <div className='drip'>DRiPP</div>
+        </div>
+
+        <div className="termsHeading">
+          <div className="termsHeadingCard">Terms and conditions</div>
+          <div className="privacyHeading">
+            <div className="privacyPolicyHeading">Privacy Policy</div>
+          </div>
+
+          <div className="privacyInputCardHeadingContainer"  >
+            <span >
+              Privacy Policy DRIPP
+            </span>
+            <div className='drippParagraphCard2'>
+              <p>
+                This policy (“Policy”, together with our Terms of Use) explains and sets out the basis for why and when we (DRIPP) collect personal information about the people <br />
+                who visit our website(s), how we use it, the conditions under which we may disclose it to others and the measures we take to keep it secure.<br />
+                By visiting this website, you are accepting and consenting to the practices described in this Policy. Please note that this includes consenting to the collection<br />
+                and processing of any personal information you provide, as described below. We may amend this Policy from time to time so please check it occasionally to <br />
+                ensure that you agree with any changes. Your continued use of our website(s) will constitute your acceptance of, and agreement to, any changes.<br /></p>
+            </div>
+            <span className='collectPrivacyInform' id='about you'>
+              How we collect information about you?
+            </span>
+            <p className='provideCardPrivacyInform'>
+              <span className='circle'>
+                <span className='dot'>&#x2022;</span>
+                Information you provide us</span><br />
+
+              You may provide us with personal information by filling in forms on our website(s) or by corresponding with us by e-mail.<br />
+              The personal information you provide may include your name, email address, phone number, IP address, language preference, and information regarding the <br />
+              pages you access.  <br />
+              By providing us with this information, you expressly consent to our use of your personal information in accordance with this Policy.
+              <br />
+              <br />
+              <span className='circle'>
+                <span className='dot'>&#x2022;</span>
+                Information we collect about you</span><br />
+
+              When you visit our website(s) we may collect information about your IP address, information about your visit, your browsing activity, and how <br />
+              you use our website. This information may be combined with other information you provide.
+              <br />
+              <br />
+              <span className='circle'>
+                <span className='dot'>&#x2022;</span>
+                Information we receive from other sources </span><br />
+
+              We may receive information about you if you use any other websites, we, or our partners, operate. We also work with third parties (including <br />
+              contractors, project partners, service providers, analytics providers) and may receive information about you from them. This may be combined<br />
+              with other information you provide to us.
+              <br />
+              <br />
+
+              <span className='collectPrivacyInform' id='information'>How your information is used </span><br />
+
+              We may use personal information about you for the following purposes:<br />
+              <li>To provide you with relevant information and news.</li>
+              <li>To send you personalized communications which you have requested and that may be of interest to you, which may be based on your activity on our website(s) or the website of our partners.</li>
+              <li>These may include information about campaigns, activities, and events.</li>
+              <li>To understand and measure the effectiveness of how we serve you and others.</li>
+              <li>To make suggestions and recommendations to you about services that may interest you, which may be based on your activity on our website(s) or the website of our partners.</li>
+              <li>For analytics and profiling to create aggregate trend reports, find out how visitors arrive at our website; which sites and pages are viewed, the responses to marketing campaigns,
+                and to determine the most effective marketing channels and messages. Seek your views or comments on the service we provide.</li>
+              <li>Notify you of changes to our service, policies, and terms of use.</li>
+              <li>We review our retention periods for personal information on a regular basis.</li>
+              <li>We will hold your personal information on our systems for as long as is necessary for the relevant activity.</li>
+              <br />
+              <br />
+
+
+              <span className='collectPrivacyInform' id='access'>Who has access to your information </span><br />
+              We will not sell or rent your information to third parties. We will not share your information with third parties for marketing purposes. <br />
+
+              We may pass your information to third-party service providers, agents, subcontractors, and other associated organizations for the purposes of completing
+              tasks and providing services to you on our behalf (for example to process and store information and to send you emails).  <br />
+              However, if we use third-party service providers, we disclose only the personal information that is necessary to deliver the service and that requires them to<br />
+
+              keep your information secure and not to use it for their own direct marketing purposes. We will take the necessary<br />
+
+              steps to ensure that your privacy rights continue to be protected.
+              <br />
+              <br />
+
+              <span className='collectPrivacyInform' id='choices'>Your choices and your rights </span><br />
+              You have the right to inform us not to process your personal information for marketing purposes. You can exercise your right and prevent us from processing such information by checking or unchecking certain boxes on the forms <br />
+              we use to collect your data. You can also exercise the right at any time by contacting us by email at hi@dripp.zone. We will not contact you for marketing purposes unless you have given your prior consent.<br />
+              You can change your marketing preferences at any time by contacting us by email at hi@dripp.zone.
+              <br />
+              <br />
+              <span className='collectPrivacyInform' id='update' >How you can update your information </span><br />
+              The accuracy of your information is important to us. If you change your email address, or any of the other information we hold is inaccurate or out of date, please email us at hi@dripp.zone. <br />
+              You have the right to ask for a copy of the information DRIPP holds about you.  <br /><br />
+
+              <span className='collectPrivacyInform' id='measures'>Security measures to protect your information </span><br />
+              When you give us personal information, we take steps to ensure that it's treated securely. Non-sensitive details such as your email address may be transmitted unencrypted over the Internet, <br />
+              and so may not be guaranteed to be 100% secure. While we strive to protect your personal information,  we cannot guarantee the security of any information you transmit to us, and you do so at your own risk. <br />
+              When we receive your information, we will make our best effort to ensure its security on our systems.<br /><br />
+
+              <span className='collectPrivacyInform' id='Newsletter'>Newsletter </span><br />
+              The provisions of this Policy also apply to the request form and sending of our newsletter. The newsletter will only be sent after a prior request by the user in the so-called “double opt-in procedure” (request on our website and confirmation of a corresponding request e-mail).
+              Users of the newsletter are also assigned a UserID, which allows DRIPP to determine when the relevant newsletter was opened and which links or functions from the relevant newsletter were activated. This tracking is done for the internal optimization of the newsletter.
+              This data will not be passed on to third parties. DRIPP has an interest in evaluating such data for purposes of newsletter optimization and, on the other hand, a concerned person can reasonably foresee at the time when the personal data is collected and in view of the
+              circumstances under which it is carried out (in particular the above-mentioned measures) that it will possibly be processed for this purpose. The user has the right to withdraw his or her consent at any time. If the user of the newsletter does not wish to receive this tracking,
+              he/she can unsubscribe from the newsletter. To do so, simply unsubscribe via the link in the newsletter or send an e-mail to hi@dripp.zone. The withdrawal of consent shall not affect the lawfulness of processing based on consent before its withdrawal.  <br /> <br />
+
+              <span className='collectPrivacyInform' id='Use of Cookies'>Use of Cookies </span><br />
+              Our site(s) use cookies to distinguish you from other visitors to our site. Cookies are small text files that may be stored on your computer or other internet-enabled devices, such as a smartphone or tablet. They are sent by a server to your computer and stored on your hard drive to allow a website to recognize you when you visit.  <br /><br />
+              Cookies can be used by websites to make the user's experience more efficient. Our site(s) use different types of cookies. Some cookies are placed by third-party services that appear on our pages. We use cookies to personalize content, provide social media features and analyze our traffic. This improves your experience when you browse our site(s) and allows us to improve the service we provide. By continuing to browse our site(s), you are agreeing to our use of cookies.  <br /><br />
+              We also share information about your use of our site with our social media and analytics partners who may combine it with other information that you have provided to them or that they have collected from your use of their services.
+              You can at any time change or withdraw your consent from the cookie declaration on our website. Your consent applies to the following domain: www.dripp.zone, www.dripp.zone/news<br /><br />
+
+              <span className='collectPrivacyInform' id='cookies'>We use the following cookies: </span><br />
+
+              <section>Strictly Necessary Cookies </section>
+              These cookies are necessary for the website to function and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences, logging in or filling in forms. These cookies enable basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.
+
+              <section>Performance Cookies </section>
+              These cookies are usually referred to as statistical cookies and allow us to count visits, traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies we will not know when you have visited our site and will not be able to monitor its performance.
+
+              <section>Links to other websites </section>
+              Our website may contain links to other websites run by other organizations. This Policy applies only to our website, so we encourage you to read the privacy statements on the other websites you visit. We cannot be responsible for the privacy policies and practices of other sites even if you access them using links from our website.  <br /><br />
+              If you linked to our website from a third-party site, we cannot be responsible for the privacy policies and practices of the owners and operators of that third-party site and recommend that you check the policy of that site.
+
+              <section>16 or Under  </section>
+              We are concerned about protecting the privacy of children aged 16 or under. If you are aged 16 or under, please get your parent/guardian's permission beforehand whenever you provide us with personal information.
+
+              <section> Disclaimer:</section>
+              Investing in crypto, including DRIPP (the Token) is high risk, and the value of your investments can go down as well as up. The purchase of cryptocurrencies is not legal in all countries/states. Please take legal/tax advice before considering any investment. DRIPP (the Company) is a non-profit organization responsible for marketing DRIPP (the Token) and cannot be held responsible for any breaches in local law or regulation as a result of you purchasing DRIPP (the Token). Nothing stated on this website or on any content produced by DRIPP (the Company) should be considered as a recommendation to invest.
+
+            </p>
+            <div className='privacyDownloadCard'>
+              Download:
+              < div className='privacy-li'>
+                <span className='pdficon'><img src={vector} />.pdf</span>
+                <span className='docicon'><img src={vector} />.doc</span>
+                <span className='rtficon'><img src={vector} />.rtf</span>
+
+                <div className='privacyContent'>
+                  Content:
+                  <p className='flex-container' >
+                    <a href='#about you'>How we collect information about you</a><br />
+                    <a href='#information'> How your information is used </a><br />
+                    <a href='#access '>Who has access to your information </a><br />
+                    <a href='#choices'>Your choices and your rights </a><br />
+                    <a href='#update'>How you can update your information </a><br />
+                    
+                    <a href='#measures'>Security measures to protect your information </a><br />
+                    <a href='#Newsletter'>Newsletter </a><br />
+                    <a href='#Use of Cookies'>Use of Cookies </a><br />
+                    <a href='#cookies'>We use the following cookies  </a> <br />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Privacy  
